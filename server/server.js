2c93ae87dcc12da3e02aa5e994e0cd8a827c575c
jsmndb <jasmindbernal@gmail.com>
2026-07-23 17:34:58 +0800

    Add initial Taskify API routes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,27 @@ const app = express();
 
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-    res.send("Hello from Tasktify API!");
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to Taskify API!",
+        version: "1.0.0",
+        status: "Running"
+    });
+});
+
+app.get("/api/tasks", (req, res) => {
+    res.json([
+        {
+            id: 1,
+            title: "Learn Express",
+            completed: false
+        },
+        {
+            id: 2,
+            title: "Build Taskify",
+            completed: false
+        }
+    ]);
 });
 
 app.listen(PORT, () => {
